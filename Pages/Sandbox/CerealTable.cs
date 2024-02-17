de8c884cc7a306b8fcdb05f4d3dabbd59ad56aa2
mikepreston17 <michael.n.preston@gmail.com>
2024-02-16 19:45:01 -0600

    initial commit

---
+++ Pages/Sandbox/CerealTable.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using CodeMechanic.RazorHAT;
+using Microsoft.AspNetCore.Html;
+
+namespace SvelteRazor.Pages.Sandbox;
+
+public class CerealTable
+{
+    public List<Cereal> Rows = new List<Cereal>()
+    {
+        new Cereal { name = "Cap'n Crunch", cost = 15.00 },
+        new Cereal { name = "Cookie Crisp", cost = 15.00 },
+
+        new Cereal
+        {
+            name = "Frankenberry",
+            cost = 16.00
+        }
+    };
+}
+
+public record Cereal
+{
+    public string name { get; set; }
+    public double cost { get; set; }
+
+    public HtmlString ToHtml()
+    {
+        return $"<td>{name}</td><td>{cost}</td>".AsHTMLString();
+    }
+}
