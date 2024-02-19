911b3d77d534c35dcf8d4fb487c551286ca5721e
mikepreston17 <michael.n.preston@gmail.com>
2024-02-18 21:58:11 -0600

    playing around with alpine intersect

--- Pages/TPOT/Index.cshtml.cs
+++ Pages/TPOT/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using CodeMechanic.Curl;
 using CodeMechanic.FileSystem;
@@ -25,10 +26,11 @@ public class Index : PageModel
     {
     }
 
-    public async Task SearchWordpress()
+    public async Task<IActionResult> OnGetSearchWordpress()
     {
+        Console.WriteLine(nameof(OnGetSearchWordpress));
+        return Partial("_ParsedAPI", null);
     }
-
 
     public async Task OnPostAsync()
     {
